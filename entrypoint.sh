6afa6a3b43ba90f46548be817e2ee464aa4cf0c1
Abdou BA <ba2664890@gmail.com>
2025-11-14 23:12:08 +0000

    ajout des fichies

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,13 @@ echo ">>> Running Airflow DB upgrade"
 airflow db migrate
 
 echo ">>> Creating admin user if not exists"
-
+airflow users create \
+    --username admin \
+    --firstname Admin \
+    --lastname User \
+    --role Admin \
+    --password admin \
+    --email admin@example.com
 
 echo ">>> Starting Airflow Webserver"
 exec airflow webserver
