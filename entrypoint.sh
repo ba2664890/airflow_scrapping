5e90190e5a000e7bbe2a820917ddc73bf02fb7b1
Abdou BA <ba2664890@gmail.com>
2025-11-15 01:01:18 +0000

    supprission des files local

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-AIRFLOW_CMD="$HOME/.local/bin/airflow"
+# Utiliser le binaire airflow global (présent dans l'image officielle)
+AIRFLOW_CMD="airflow"
 
 echo ">>> Running Airflow DB upgrade"
-$AIRFLOW_CMD db migrate
+$AIRFLOW_CMD db upgrade
 
 echo ">>> Creating admin user if not exists"
 $AIRFLOW_CMD users create \
@@ -13,7 +14,7 @@ $AIRFLOW_CMD users create \
     --lastname User \
     --role Admin \
     --password admin \
-    --email admin@example.com
+    --email admin@example.com || true
 
 echo ">>> Starting Airflow Webserver"
 export WEB_SERVER_MASTER_TIMEOUT=300
