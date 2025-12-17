338ffc038b9104ff71ad7c8275ad318a98a61ed6
Abdou Ba <ba2664890@gmail.com>
2025-12-17 19:30:34 +0000

    ajout du scheduler

--- entrypoint-scheduler.sh
+++ entrypoint-scheduler.sh
@@ -19,8 +19,6 @@ airflow users create \
     --password admin \
     --email admin@example.com || true
 
-# --- IMPORTANT FOR RAILWAY: disable scheduler in web dyno ---
-echo ">>> Starting Airflow Webserver ONLY (Railway Dyno)"
 export AIRFLOW__CORE__LOAD_EXAMPLES=False
 export AIRFLOW__WEBSERVER__WORKERS=1
 export AIRFLOW__WEBSERVER__WEB_SERVER_MASTER_TIMEOUT=300
