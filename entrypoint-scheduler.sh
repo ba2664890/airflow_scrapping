9e320c7dc131b930bfa56c07f6a24086015f829b
Abdou Ba <ba2664890@gmail.com>
2025-12-19 04:37:48 +0000

    ajout des modif

--- entrypoint-scheduler.sh
+++ entrypoint-scheduler.sh
@@ -1,27 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo ">>> PATH: $PATH"
-which airflow || true
-airflow version
-
-# --- DATABASE INIT ---
-echo ">>> Running Airflow DB migration (upgrade)"
-airflow db migrate 
-
-# --- CREATE ADMIN USER ---
-echo ">>> Creating admin user if not exists"
-airflow users create \
-    --username admin \
-    --firstname Admin \
-    --lastname User \
-    --role Admin \
-    --password admin \
-    --email admin@example.com || true
-
-export AIRFLOW__CORE__LOAD_EXAMPLES=False
-export AIRFLOW__WEBSERVER__WORKERS=1
-export AIRFLOW__WEBSERVER__WEB_SERVER_MASTER_TIMEOUT=300
 
 echo ">>> Starting Airflow Webserver in DEBUG MODE (no gunicorn, stable for Railway)"
 exec airflow scheduler
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,26 +5,7 @@ echo ">>> PATH: $PATH"
 which airflow || true
 airflow version
 
-# --- DATABASE INIT ---
-echo ">>> Running Airflow DB migration (upgrade)"
-airflow db reset -y
-airflow db migrate 
 
-# --- CREATE ADMIN USER ---
-echo ">>> Creating admin user if not exists"
-airflow users create \
-    --username admin \
-    --firstname Admin \
-    --lastname User \
-    --role Admin \
-    --password admin \
-    --email admin@example.com || true
-
-# --- IMPORTANT FOR RAILWAY: disable scheduler in web dyno ---
-echo ">>> Starting Airflow Webserver ONLY (Railway Dyno)"
-export AIRFLOW__CORE__LOAD_EXAMPLES=False
-export AIRFLOW__WEBSERVER__WORKERS=1
-export AIRFLOW__WEBSERVER__WEB_SERVER_MASTER_TIMEOUT=300
 
 echo ">>> Starting Airflow Webserver in DEBUG MODE (no gunicorn, stable for Railway)"
 exec airflow webserver --port 8080 --debug
